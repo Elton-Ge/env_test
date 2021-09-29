05a09e3ffcfeeabf4127f63e30ae568045434be2
ELTON-GE <gexin7648@hotmail.com>
2021-09-29 22:26:44 +1000

    update_env_test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.get("/welcome",(req, res) => {
     res.send({message: 'Welcome'})
 })
-
+console.log(process.env.DBHOST,"for_testing");
 const PORT=  process.env.PORT || 5050;
 console.log(PORT,"localPORT");
 app.listen(PORT,() => {
